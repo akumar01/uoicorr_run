a0459ab8f19893fa53b74f4729e91a45113b0914
akumar01 <akumar.4db0@gmail.com>
2020-01-10 15:47:41 -0800

    adding script reintegrate bad children

---
+++ reintegrate_bad_children.py
@@ -0,0 +1,47 @@
+import sys, os
+import pdb
+import glob
+import argparse
+import pickle
+
+import h5py_wrapper 
+
+from job_utils.results import ResultsManager
+from job_utils.results import insert_data
+
+# Run this script after running discipline_bad_children.py, in turn after running parallel_concatenate
+# and identifying the children that could not be integrated into the concatenated data files
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('source_dir')
+    parser.add_argument('exp_type')
+
+    args = parser.parse_args()
+
+    # Grab the bad children
+
+    # Grab the bad children
+    bad_children_files = glob.glob('%s/bad_children*' % args.source_dir)    
+
+    bad_children = []
+    for bad_children_file in bad_children_files:
+        with open(bad_children_file, 'rb') as f:
+            bad_children.extend(pickle.load(f))
+
+   	# Iterate through the corresponding data files, load the child data, and insert it into the 
+   	# right place
+   	for bad_child in bad_children:
+
+   		# Assemble full path to bad_child path
+   		child_path = source_dir + '/' + exp_type + '/' + bad_child['path']
+   		child_data = h5py_wrapper.load(child_path)
+
+   		# Load master file
+   		child_dir = bad_child['path'].split('/child')[0]
+   		master_path = source_dir + '/' + exp_type + '/' + child_dir + '.dat'
+   		master_data = h5py_wapper.load(master_path)
+   		pdb.set_trace()
+   		master_data = insert_data(master_data, child_data, child['idx'])
+   		pdb.set_trace()
+   		#h5py_wapper.save(master_path, master_data, write_mide)
