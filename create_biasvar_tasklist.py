ec23d11d9d5e68e5bb68cbb06b5ac74b42342092
akumar01 <akumar.4db0@gmail.com>
2020-02-04 15:09:39 -0800

    debugged create_biasvar_tasklist.py

--- create_biasvar_tasklist.py
+++ create_biasvar_tasklist.py
@@ -5,6 +5,7 @@ import time
 import pdb
 import pandas as pd
 import sqlalchemy
+import h5py, itertools
 from mpi4py import MPI
 from schwimmbad import MPIPool
 
@@ -17,10 +18,14 @@ from postprocess_utils import *
 root_dir = os.environ['SCRATCH']
 
 # Read the non-concatenated dataframes to ensure indices are properly preserved
-lasso = pd.read_pickle('%s/finalfinal/lasso_df.dat' % root_dir)
-mcp = pd.read_pickle('%s/finalfinal/mcp_df.dat' % root_dir)
-scad = pd.read_pickle('%s/finalfinal/scad_df.dat' % root_dir)
-en = pd.read_pickle('%s/finalfinal/en_df.dat' % root_dir)
+lasso = pd.read_pickle('%s/finalall/lasso_df.dat' % root_dir)
+print('Read lasso')
+mcp = pd.read_pickle('%s/finalall/mcp_df.dat' % root_dir)
+print('Read mcp')
+scad = pd.read_pickle('%s/finalall/scad_df.dat' % root_dir)
+print('Read scad')
+en = pd.read_pickle('%s/finalall/en_df.dat' % root_dir)
+print('Read EN')
 
 # Remove the parasitic index field
 lasso = lasso.drop('index', axis=1)
@@ -43,16 +48,18 @@ kappa = 5
 np_ratio = 4
 cov_idxs = np.arange(80)
 
-beta_fnames = ['%s/finalfinal/%s_pp_beta.h5' % (root_dir, dfname) for dfname in ['lasso', 'mcp', 'scad', 'en']]
+beta_fnames = ['%s/finalall/%s_pp_beta.h5' % (root_dir, dfname) for dfname in ['lasso', 'mcp', 'scad', 'en']]
 beta_files = [h5py.File(beta_fname, 'r') for beta_fname in beta_fnames]
 
 param_combos = list(itertools.product(sparsity, betawidth, selection_methods, cov_idxs))
+
+print('Initialized param_combos')
 
 # Arrange tasks from param combos 
 task_list = []
 for i, dframe in enumerate(dframes):
     for param_comb in param_combos:
-        s, bw, sm, cidx = param_combo
+        s, bw, sm, cidx = param_comb
         df = apply_df_filters(dframe, sparsity=s, betawidth=bw, 
                                   selection_method=sm, cov_idx=cidx, kappa=kappa, np_ratio=np_ratio)
         if df.shape[0] == 0:
